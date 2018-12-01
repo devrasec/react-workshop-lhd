441ed5e7fdadaffb1f7dfad6de8118cf8d12326e
Luis Contreras <one.tmac@gmail.com>
2018-12-01 01:24:45 -0400

    Show form to add todos only when the username is added
    Other changes:
    
    - Persist the username in the SessionStorage

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import UsernameForm from './components/UsernameForm';
 import Check from './components/Check';
 import AddTodoForm from './components/AddTodoForm';
 
+const userNameKey = 'todo-username';
+
 class App extends PureComponent {
   state = {
     todos: [],
     username: ''
   };
 
-  handleUserNameChange = username => {
-    this.setState({ username });
+  componentDidMount() {
+    if (sessionStorage.getItem(userNameKey) !== null) {
+      this.setState({ username: sessionStorage.getItem(userNameKey) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const persistedUsername = sessionStorage.getItem(userNameKey);
+    const { username } = this.state;
+
+    if (
+      (persistedUsername === null && username) ||
+      prevState.username !== username
+    ) {
+      sessionStorage.setItem(userNameKey, username);
+    }
+  }
+
+  onUsernameAdd = username => {
+    this.setState({ username, userNameAdded: true });
   };
 
   addToDo = todoTitle => {
@@ -27,39 +47,49 @@ class App extends PureComponent {
 
     return (
       <div className="app container">
-        <UsernameForm
-          username={username}
-          onChange={this.handleUserNameChange}
-        />
+        {!username && <UsernameForm onSubmit={this.onUsernameAdd} />}
 
-        <AddTodoForm onAdd={this.addToDo} />
-
-        <div>
-          {todos.map((todo, index) => (
-            <div
-              key={index}
-              className="card mb-3 shadow-sm rounded border-light"
-            >
-              <div className="card-body">
-                <div className="row align-items-center">
-                  <div className="col-2 col-sm-1">
-                    <Check />
-                  </div>
-                  <div className="col-8 col-sm">{todo.title}</div>
-                  <div className="col-2 col-sm-1">
-                    <button
-                      type="button"
-                      className="close"
-                      aria-label="Eliminar"
-                    >
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                </div>
+        {username && (
+          <>
+            <div class="jumbotron">
+              <div class="container">
+                <h1 class="display-4">Hola, {username}!</h1>
+                <p class="lead">
+                  Ahora puedes agregar las cosas que tienes pendientes de hacer.
+                </p>
               </div>
             </div>
-          ))}
-        </div>
+
+            <AddTodoForm onAdd={this.addToDo} />
+
+            <div>
+              {todos.map((todo, index) => (
+                <div
+                  key={index}
+                  className="card mb-3 shadow-sm rounded border-light"
+                >
+                  <div className="card-body">
+                    <div className="row align-items-center">
+                      <div className="col-2 col-sm-1">
+                        <Check />
+                      </div>
+                      <div className="col-8 col-sm">{todo.title}</div>
+                      <div className="col-2 col-sm-1">
+                        <button
+                          type="button"
+                          className="close"
+                          aria-label="Eliminar"
+                        >
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
       </div>
     );
   }
--- src/components/UsernameForm.js
+++ src/components/UsernameForm.js
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import classNames from 'classnames';
 import TextInput from './TextInput';
 
-export default function UsernameForm({ username, onChange }) {
-  return (
-    <div className="mb-4">
-      <TextInput
-        label="Nombre de usuario"
-        name="username"
-        value={username}
-        placeholder="¿Cómo te llaman tus amigos?"
-        onChange={event => onChange(event.target.value)}
-      />
+export default class UsernameForm extends Component {
+  state = {
+    username: '',
+    error: ''
+  };
 
-      <button type="button" className="btn btn-primary">
-        Continuar
-      </button>
-    </div>
-  );
+  handleSubmit = event => {
+    event.preventDefault();
+    const { username } = this.state;
+
+    if (!username.trim()) {
+      this.setState({
+        error: 'El nombre de usuario es requerido',
+        username: ''
+      });
+    } else {
+      this.props.onSubmit(this.state.username);
+    }
+  };
+
+  handleChange = event => {
+    this.setState({ username: event.target.value, error: '' });
+  };
+
+  render() {
+    const { username, error } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="mb-4">
+          <TextInput
+            label="Nombre de usuario"
+            name="username"
+            value={username}
+            placeholder="¿Cómo te llaman tus amigos?"
+            onChange={this.handleChange}
+            error={error}
+          />
+
+          <button type="submit" className="btn btn-primary">
+            Continuar
+          </button>
+        </div>
+      </form>
+    );
+  }
 }
